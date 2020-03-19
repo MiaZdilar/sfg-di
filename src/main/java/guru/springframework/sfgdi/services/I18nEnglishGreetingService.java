d3e4603fa60f49906691561f18288b36ac5341c4
Mia Zdilar <mia.zdilar.posao@gmail.com>
2020-03-19 17:38:29 +0100

    Change - controllers, services, properties

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Service;
 @Profile("EN")
 @Service("i18nService")
 public class I18nEnglishGreetingService implements GreetingService {
-
     @Override
     public String sayGreeting() {
         return "Hello World - EN!";
     }
+
 }
--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 @Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService {
-
     @Override
     public String sayGreeting() {
         return "Hola Mundo - ES!";
